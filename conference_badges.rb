3cf690a6a8b2eaf503aa49a48b8992edfc11ed5f
dmadust@gmail.com <dmadust@gmail.com>
2018-05-17 20:31:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,4 +10,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(speakers)
+  assignments = []
+  speakers.each_with_index{|speaker, index| assignments << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"}
+  assignments
 end
