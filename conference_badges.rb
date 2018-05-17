2aaf8ca861a73a28e620f8bd62f71dccb5f1661e
dmadust@gmail.com <dmadust@gmail.com>
2018-05-17 20:27:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
 def batch_badge_creator(names)
   badge_messages = []
-  names.each{|name| badge_messages << "Hello, my name is #{name}."}
+  names.each{|name| badge_messages << badge_maker(name)}
   badge_messages
 end
